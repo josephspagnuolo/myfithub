b27c552bae192b3d6e72529ecd8b7761b94d3318
spagnuolo-joseph-216279184 <josephspagnuolo1@gmail.com>
2023-09-11 23:18:53 -0400

    Update dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -28,6 +28,12 @@ export default function Home() {
             Protected Page
           </Link>
         </div>
+        <div className="text-center max-w-screen-sm mb-10">
+          <p className="text-stone-400 mt-5">
+            {`The date today is ${new Date().toDateString()} `}
+            {`The date one year ago was ${new Date(new Date().setFullYear(new Date().getFullYear() - 1))}`}
+          </p>
+        </div>
       </div>
     </div>
   );
